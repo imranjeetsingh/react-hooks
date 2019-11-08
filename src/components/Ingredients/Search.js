57ab74cbde25a5929e2a9c4a05ca1a66a66b6e59
imranjeetsingh <ranjee970@gmail.com>
2019-11-08 12:16:57 +0530

    used useRef()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,32 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const {onLoadIngredients} = props
   const [enteredFilter, setEnteredFilter] = useState('')
+  const inputRef = useRef();
 
-  useEffect(() =>{
-    const query = 
-                  enteredFilter.length === 0 ? '' :
-                  `?orderBy="title"&equalTo="${enteredFilter}"`
-    fetch('https://react-burger-app-ab541.firebaseio.com/orders.json'+query)
-    .then(response => response.json())
-    .then(responseData =>{
-      const ingredientsLoaded = []
-      for(const ing in responseData){
-        ingredientsLoaded.push({
-          id: ing,
-          title : responseData[ing].title,
-          amount : responseData[ing].amount
-        })
+  useEffect(() => {
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0 ? '' :
+            `?orderBy="title"&equalTo="${enteredFilter}"`
+        fetch('https://react-burger-app-ab541.firebaseio.com/orders.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            const ingredientsLoaded = []
+            for (const ing in responseData) {
+              ingredientsLoaded.push({
+                id: ing,
+                title: responseData[ing].title,
+                amount: responseData[ing].amount
+              })
+            }
+            onLoadIngredients(ingredientsLoaded);
+          })
       }
-      onLoadIngredients(ingredientsLoaded);
-    })
-  },[enteredFilter,onLoadIngredients])
+
+    }, 500)
+
+  }, [enteredFilter, onLoadIngredients,inputRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" 
+          <input type="text"
+            ref ={inputRef} 
             value = {enteredFilter}
             onChange = {event => setEnteredFilter(event.target.value)}
           />
